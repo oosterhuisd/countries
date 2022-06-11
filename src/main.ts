4712d5b0203ee0b558a3d89afc90a6143ecf0623
danieloosterhuis <daniel@pigoly.com>
2022-06-11 11:36:28 +0200

    Add Windows flag support

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/style.css'
+import { polyfillCountryFlagEmojis } from "country-flag-emoji-polyfill";
+
+// Fixes flag support for Windows, because Windows does not support flag emojis (yes omg)
+// https://github.com/talkjs/country-flag-emoji-polyfill
+polyfillCountryFlagEmojis();
 
 createApp(App).mount('#app')
